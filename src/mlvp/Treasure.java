4614cb0c0da565cf1cd6c3b3971a1b866bcf64ed
Valentin Perignon <valentin.perignon@orange.fr>
2020-04-14 14:55:44 +0200

    Ajout de Treasure

--- src/mlvp/Treasure.java
+++ src/mlvp/Treasure.java
@@ -1,5 +1,19 @@
 package mlvp;
 
-public class Treasure {
+public class Treasure extends Cell {
+	// ----- Constructeur -----
+	
+	public Treasure(Position p) {
+		pos =  p;
+	}
 
+	// ----- Fonctions -----
+	
+	public void process(Hunter h) {
+		h.setPos(pos);
+	}
+
+	public String getSymbol() {
+		return "T";
+	}
 }
