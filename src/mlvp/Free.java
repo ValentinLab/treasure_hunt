ca552a1e05e4e016139bcae87dd20ece12208661
Medhi Louison <medhi.louison@icloud.com>
2020-04-14 15:24:21 +0100

    ajout de free et stone

--- src/mlvp/Free.java
+++ src/mlvp/Free.java
@@ -1,5 +1,27 @@
 package mlvp;
 
-public class Free {
-
+public class Free extends Cell {
+	boolean isTaken;
+	
+	Free(Position pos){
+		this.pos = pos;
+	}
+	
+	public void setTaken(){
+		isTaken = !isTaken;
+	}
+	public String getSymbol() {
+		return(".");
+	}
+	
+	public void process(Hunter h) {
+		if(isTaken) {
+			h.getDir().setDir();
+			h.setPos(pos);
+		}
+		else {
+			h.getDir().setOpposite();
+		}
+	}
+	
 }
--- src/mlvp/Stone.java
+++ src/mlvp/Stone.java
@@ -1,5 +1,16 @@
 package mlvp;
 
-public class Stone {
+public class Stone extends Cell {
+	
+	Stone(Position pos){
+		this.pos = pos;
+	}
+	public String getSymbol() {
+		return("#");
+	}
+	public void process(Hunter h) {
+		h.getDir().setNear();
+	}
 
 }
+
